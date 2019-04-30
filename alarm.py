0c5729d7c501e541429ef23eb1d7115f5aaebc3e
aj <andrewjpack@gmail.com>
2019-04-30 23:56:55 +0100

    added checking for phone

--- alarm.py
+++ alarm.py
@@ -3,10 +3,27 @@ import spotframework.net.network as networkclass
 
 import os
 
+def checkPhone():
+
+    response = os.system("ping -c 1 -w5 " + os.environ['PHONEIP'] + " > /dev/null 2>&1")
+    print('checking for phone')
+    if response == 0:
+        return True
+    else:
+        return False
+
 if __name__ == '__main__':
     network = networkclass.network(userclass.User())
 
-    network.play(os.environ['SPOTALARMURI'], network.getDeviceID(os.environ['SPOTALARMDEVICENAME']))
+    found = False
 
-    network.setShuffle(True)
-    network.next()
+    for i in range(0, 36):
+        if checkPhone():
+            found = True
+            break
+
+    if found:
+        network.play(os.environ['SPOTALARMURI'], network.getDeviceID(os.environ['SPOTALARMDEVICENAME']))
+
+        network.setShuffle(True)
+        network.next()
